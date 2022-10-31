744c6744052f7d92bc204d4ece78462263a6cd4f
justinnikolic <117117011+justinnikolic@users.noreply.github.com>
2022-10-31 17:58:50 -0400

    Add files via upload

---
+++ Image Processor/src/Model/ImageModel.java
@@ -0,0 +1,15 @@
+package Model;
+
+import java.util.ArrayList;
+
+public class ImageModel {
+
+  private int[] pixel;
+  private Pixel[][] picture;
+
+  public ImageModel(Pixel[][] picture) {
+    this.picture = picture;
+  }
+
+
+}
---
+++ Image Processor/src/Model/Pixel.java
@@ -0,0 +1,34 @@
+package Model;
+
+
+public class Pixel {
+  private int red;
+  private int blue;
+  private int green;
+
+  private int luma;
+
+  public Pixel(int red, int blue, int green) {
+    this.red = red;
+    this.blue = blue;
+    this.green = green;
+  }
+
+  public int red() {
+    return this.red;
+  }
+
+  public int blue() {
+    return this.blue;
+  }
+
+  public int green() {
+    return this.green;
+  }
+
+  public int getLuma() {
+    return
+  }
+
+
+}
